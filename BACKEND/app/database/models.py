b465d85d568ce832ac0dc526e92dd2cb639eeba6
Bernise.john <bernise.john@bridgeconn.com>
2025-07-22 16:11:10 +0530

    folderstructureandfastapi

---
+++ BACKEND/app/database/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, func
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(120), unique=True, nullable=False, index=True)
+    hashed_password = Column(String(128), nullable=False)
+    full_name = Column(String(100), nullable=True)
+    is_active = Column(Boolean, default=True)
+    is_superuser = Column(Boolean, default=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
