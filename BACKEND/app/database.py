3896330c02c064d07ae6071e553edb0a8b13cb50
Bernise.john <bernise.john@bridgeconn.com>
2025-07-23 16:29:45 +0530

    added dotenv

--- BACKEND/app/database.py
+++ BACKEND/app/database.py
@@ -1,10 +1,34 @@
-from sqlalchemy import create_engine
+import os
+import urllib
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, MetaData, inspect
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.schema import CreateSchema
 
-DATABASE_URL = "postgresql://bibleuser:biblepass123@localhost/ai_translator"
+load_dotenv()
 
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_SCHEMA = os.getenv("POSTGRES_SCHEMA")
 
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = (
+    f"postgresql+psycopg2://{POSTGRES_USER}:{urllib.parse.quote(POSTGRES_PASSWORD)}"
+    f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+)
+
+engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20)
+
+with engine.connect() as connection:
+    if not connection.dialect.has_schema(connection, POSTGRES_SCHEMA):
+        connection.execute(CreateSchema(POSTGRES_SCHEMA))
+        connection.commit()
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
+Base.metadata = MetaData(schema=POSTGRES_SCHEMA)
+inspector = inspect(engine)
