9d7e8f8f37b7ecd7b5b8256ea7d078359c307569
Adity Singh <adity.singh@bridgeconn.com>
2025-07-25 17:19:15 +0530

    adding codes after feedback

--- BACKEND/app/routes/users.py
+++ BACKEND/app/routes/users.py
@@ -1,14 +1,18 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app.schemas.users import UserCreate, UserResponse
+from app.schemas.users import UserCreate, ErrorResponse, SuccessResponse
 from app.crud.users import user_service
 
 router = APIRouter()
 
 @router.post(
     "/",
-    response_model=UserResponse,
+    response_model=SuccessResponse,
+    responses={
+        409: {"model": ErrorResponse}
+    },
+ 
     status_code=status.HTTP_201_CREATED,
     summary="Create a new user",    
 )
@@ -18,4 +22,7 @@ def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
     The password will be hashed before storage.
     """
     db_user = user_service.create_user(db=db, user=user)
-    return db_user
+    return {
+        'message': "User Created Successfully",
+        "data": db_user
+    }
