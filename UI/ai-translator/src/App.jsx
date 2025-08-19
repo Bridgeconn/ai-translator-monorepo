fa28dfaf7e89d15fc324131a1961ed7d71d117a9
Joel.Johnson <joelc.johnson@bridgeconn.com>
2025-08-19 10:45:53 +0530

    added the zustand store

--- UI/ai-translator/src/App.jsx
+++ UI/ai-translator/src/App.jsx
@@ -1,8 +1,14 @@
+import useStore from "./store/useStore";
+import ZustandExample from "./ZustandExample";
 function App() {
+  const bearCount = useStore((state) => state.bearCount);
+  const user = useStore((state) => state.user);
+  const increaseBearCount = useStore((state) => state.increaseBearCount);
+  const fetchUser = useStore((state) => state.fetchUser);
 
   return (
     <>
-      <h1 class="text-3xl font-bold">Hello world!</h1>
+      <ZustandExample />
     </>
   );
 }
