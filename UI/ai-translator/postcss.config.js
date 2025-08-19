3328bfaa42deda9e5a3003c84f2cd6bbae6d8bdc
Joel.Johnson <joelc.johnson@bridgeconn.com>
2025-08-19 13:07:32 +0530

    added ant design

--- UI/ai-translator/postcss.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {}, // Corrected plugin name
-    'autoprefixer': {},
-  },
-}
--- UI/ai-translator/src/App.jsx
+++ UI/ai-translator/src/App.jsx
@@ -1,14 +1,13 @@
-import useStore from "./store/useStore";
-import ZustandExample from "./ZustandExample";
+import DefaultLayout from "./components/Layout";
 function App() {
-  const bearCount = useStore((state) => state.bearCount);
-  const user = useStore((state) => state.user);
-  const increaseBearCount = useStore((state) => state.increaseBearCount);
-  const fetchUser = useStore((state) => state.fetchUser);
+  // const bearCount = useStore((state) => state.bearCount);
+  // const user = useStore((state) => state.user);
+  // const increaseBearCount = useStore((state) => state.increaseBearCount);
+  // const fetchUser = useStore((state) => state.fetchUser);
 
   return (
     <>
-      <ZustandExample />
+      <DefaultLayout />
     </>
   );
 }
