e11add9fdfab81e69d31cc92cd4f3f1d5cc72945
Joel C Johnson <joelcjohnson123@gmail.com>
2025-08-27 07:53:43 +0530

    Updated the code

--- BACKEND/app/main.py
+++ BACKEND/app/main.py
@@ -56,16 +56,9 @@ def ping_db(db: Session = Depends(get_db)):
     
 from fastapi.middleware.cors import CORSMiddleware
 
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:5173",   # add this
-    "http://127.0.0.1:5173",   # optional
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
